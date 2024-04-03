26d27132b25d0d804fb93396f452471a5b941101
unknown <adityagps0@gmail.com>
2024-04-03 23:35:52 +0700

    Modul 6 Laravel

--- PF_Modul-4/resources/views/employee/edit.blade.php
+++ PF_Modul-4/resources/views/employee/edit.blade.php
@@ -10,7 +10,7 @@
 </head>
 
 <body>
-    <nav class="navbar navbar-expand-md navbar-dark bg-primary">
+    {{-- <nav class="navbar navbar-expand-md navbar-dark bg-primary">
         <div class="container">
             <a href="{{ route('home') }}" class="navbar-brand mb-0 h1"><i class="bi-hexagon-fill me-2"></i> Data
                 Master</a>
@@ -35,16 +35,16 @@
                         class="bi-person-circle me-1"></i> My Profile</a>
             </div>
         </div>
-    </nav>
+    </nav> --}}
 
-    <div class="container-sm mt-5">
-        <form action="{{ route('employees.update',  ['employee' => $employee->id]) }}" method="POST">
+    {{-- <div class="container-sm mt-5"> --}}
+    {{-- <form action="{{ route('employees.update',  ['employee' => $employee->id]) }}" method="POST">
             @csrf
             @method('PUT')
             <div class="row justify-content-center">
-                <div class="p-5 bg-light rounded-3 border col-xl-6">
+                <div class="p-5 bg-light rounded-3 border col-xl-6"> --}}
 
-                    {{-- @if ($errors->any())
+    {{-- @if ($errors->any())
                         @foreach ($errors->all() as $error)
                             <div class="alert alert-danger alert-dismissible fade show">
                                 {{ $error }}
@@ -54,7 +54,7 @@
                         @endforeach
                     @endif --}}
 
-                    <div class="mb-3 text-center">
+    {{-- <div class="mb-3 text-center">
                         <i class="bi-person-circle fs-1"></i>
                         <h4>Edit Employee</h4>
                     </div>
@@ -129,8 +129,106 @@
                     </div>
                 </div>
             </div>
-        </form>
-    </div>
+        </form> --}}
+
+    @extends('layouts.app')
+
+    @section('content')
+        <div class="container-sm mt-5">
+            <form action="{{ route('employees.update', ['employee' => $employee->id]) }}" method="POST">
+                @csrf
+                @method('put')
+                <div class="row justify-content-center">
+                    <div class="p-5 bg-light rounded-3 col-xl-6">
+                        <div class="mb-3 text-center">
+                            <i class="bi-person-circle fs-1"></i>
+                            <h4>Edit Employee</h4>
+                        </div>
+                        <hr>
+                        <div class="row">
+                            <div class="col-md-6 mb-3">
+                                <label for="firstName" class="form-
+            label">First Name</label>
+                                <input class="form-control @error('firstName') is-invalid @enderror" type="text"
+                                    name="firstName" id="firstName"
+                                    value="{{ $errors->any() ? old('firstName') : $employee->firstname }}"
+                                    placeholder="Enter First Name">
+                                @error('firstName')
+                                    <div class="text-danger"><small>{{ $message }}</small></div>
+                                @enderror
+
+                            </div>
+                            <div class="col-md-6 mb-3">
+                                <label for="lastName" class="form-label">Last Name</label>
+
+                                <input class="form-control @error('lastName')
+
+            is-invalid @enderror"
+                                    type="text" name="lastName" id="lastName"
+                                    value="{{ $errors->any() ? old('lastName') : $employee->lastname }}"
+                                    placeholder="Enter Last Name">
+                                @error('lastName')
+                                    <div class="text- danger"><small>{{ $message }}</small></div>
+                                @enderror
+                            </div>
+                            <div class="col-md-6 mb-3">
+                                <label for="email" class="form-label">Email</label>
+                                <input class="form-control @error('email')
+            is-invalid @enderror" type="text"
+                                    name="email" id="email"
+                                    value="{{ $errors->any() ? old('email') : $employee->email }}"
+                                    placeholder="Enter Email">
+                                @error('email')
+                                    <div class="text- danger"><small>{{ $message }}</small></div>
+                                @enderror
+                            </div>
+                            <div class="col-md-6 mb-3">
+                                <label for="age" class="form-label">Age</label>
+                                <input class="form-control @error('age') is-
+            invalid @enderror" type="text"
+                                    name="age" id="age" value="{{ $errors->any() ? old('age') : $employee->age }}"
+                                    placeholder="Enter Age">
+                                @error('age')
+                                    <div class="text- danger"><small>{{ $message }}</small></div>
+                                @enderror
+                            </div>
+                            <div class="col-md-12 mb-3">
+                                <label for="position" class="form-label">Position</label>
+
+                                @php
+                                    $selected = '';
+                                    if ($errors->any()) {
+                                        $selected = old('position');
+                                        $selected = $employee->position_id;
+                                } @endphp
+
+                                <select name="position" id="position"class="form-select">
+                                    @foreach ($positions as $position)
+                                        <option value="{{ $position->id }}"
+                                            {{ $selected == $position->id ? 'selected' : '' }}>
+                                            {{ $position->code . ' - ' . $position->name }}</option>
+                                    @endforeach
+                                </select>
+                                @error('position')
+                                    <div class="text- danger"><small>{{ $message }}</small></div>
+                                @enderror
+                            </div>
+                        </div>
+                        <hr>
+                        <div class="row">
+                            <div class="col-md-6 d-grid">
+                                <a href="{{ route('employees.index') }}" class="btn btn-outline-dark btn-lg mt-3"><i
+                                        class="bi-arrow-left-circle me-2"></i> Cancel</a>
+                            </div>
+                            <div class="col-md-6 d-grid">
+                                <button type="submit" class="btn btn-dark btn-lg mt-3"><i class="bi-check-circle me-2"></i>Edit</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </form>
+        </div>
+    @endsection
 
     @vite('resources/js/app.js')
 </body>
